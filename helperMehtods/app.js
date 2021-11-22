a1efa5d882783735c101d2004125843f277cbcf8
RustamovRRR <rustamrrustamov@gmail.com>
2021-11-22 23:56:52 +0400

    working with helper methods, document.write(), alert and console.log

---
+++ helperMehtods/app.js
@@ -0,0 +1,6 @@
+// less popular and useful one
+document.write("hello world with document.write");
+// most annoying one
+alert("hello with alert");
+// most useful and popular one
+console.log("hello world");
