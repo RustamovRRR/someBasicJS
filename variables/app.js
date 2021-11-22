d3fda9d9fd96b04d9f262c5ab937defb707b45d0
RustamovRRR <rustamrrustamov@gmail.com>
2021-11-23 01:54:19 +0400

    variable naming camelCase snake_case etc

--- variables/app.js
+++ variables/app.js
@@ -17,3 +17,18 @@ city = "Kiev";
 country = "Ukraine";
 sea = "Black Sea";
 console.log(city, country, sea);
+
+//variables names
+//can contain digits, letters, $
+let random123_$;
+//must start with letter, $ and _
+let variableName;
+//no keyword
+//you cant use js keywords
+//cannot start with number
+//case sensitive personName vs personname
+let oneway;
+let oneWay;
+//camelCase vs snake_case
+let camelCase;
+let snake_case;
