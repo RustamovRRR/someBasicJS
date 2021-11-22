b71961148949fd6515a3ff884952d2efabc73541
RustamovRRR <rustamrrustamov@gmail.com>
2021-11-23 00:37:51 +0400

    working with variables in js

---
+++ variables/app.js
@@ -0,0 +1,19 @@
+let personName = "Nelson Mandela";
+
+console.log(personName);
+// some fantastic code here
+console.log(personName);
+// some fantastic code here
+console.log(personName);
+// some fantastic code here
+console.log(personName);
+// some fantastic code here
+console.log(personName);
+// some fantastic code here
+
+//assigning variable values later
+let city, country, sea;
+city = "Kiev";
+country = "Ukraine";
+sea = "Black Sea";
+console.log(city, country, sea);
