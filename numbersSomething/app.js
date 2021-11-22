eb3e54a2af746e7418c41f55fea2fa5918ce6354
RustamovRRR <rustamrrustamov@gmail.com>
2021-11-23 03:46:38 +0400

    working with numbers

---
+++ numbersSomething/app.js
@@ -0,0 +1,20 @@
+// js is loosely typed - don't declare type
+
+const number = 34;
+console.log(number);
+
+let anotherNumb = 23.23;
+console.log(anotherNumb);
+anotherNumb = "js";
+console.log(anotherNumb);
+
+let number1 = 45;
+let number2 = 15;
+let add = number1 + number2;
+console.log(add);
+let sub = number1 - number2;
+console.log(sub);
+let mult = number1 * number2;
+console.log(mult);
+let div = number1 / number2;
+console.log(div);
