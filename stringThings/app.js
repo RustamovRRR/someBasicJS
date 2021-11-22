f410fa71cf580d5e11b7b31e43ceb9b756875456
RustamovRRR <rustamrrustamov@gmail.com>
2021-11-23 02:23:54 +0400

    working with strings concatinate and backsticks

---
+++ stringThings/app.js
@@ -0,0 +1,20 @@
+const couse = "john's course is the best";
+
+//string concatination
+const firstName = "James";
+const lastName = "Bond";
+const fullName = firstName + " " + lastName;
+console.log("your name is " + fullName);
+
+//website example
+const website = "facebook";
+const yourURL = "https://www." + website + ".com";
+console.log(yourURL);
+
+//using backtics in website
+const mostSpecialWebsite = `https://www.${website}.com`;
+console.log(mostSpecialWebsite);
+
+//using backtics in fullname
+const anotherFullName = `your name is ${firstName} ${lastName}`;
+console.log(anotherFullName);
