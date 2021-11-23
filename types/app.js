7c9eae897b66f64872aa9ba48ed3a53ae3abe9b6
RustamovRRR <rustamrrustamov@gmail.com>
2021-11-24 03:08:16 +0400

    data types in js

--- types/app.js
+++ types/app.js
@@ -17,3 +17,29 @@ number1 = parseInt(number1);
 number2 = parseInt(number2);
 result = number1 + number2;
 console.log(result);
+
+//7 data types
+// Primitives - string, number, boolean, null, undefined, symbol
+// objects - arrays, functions, objects
+
+//type of operator
+
+//string
+let text = "some text";
+console.log(typeof text);
+
+//number
+let someNumber = 12;
+console.log(typeof someNumber);
+
+//boolean
+let someBoolean = true;
+console.log(typeof someBoolean);
+
+//null
+let someNull = null;
+console.log(typeof someNull);
+
+//undefined
+let someUndefined;
+console.log(typeof someUndefined);
