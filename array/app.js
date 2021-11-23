9a3fb57b4273a137e620814f2bd04b5f1b288835
RustamovRRR <rustamrrustamov@gmail.com>
2021-11-24 03:42:01 +0400

    basic of array

---
+++ array/app.js
@@ -0,0 +1,16 @@
+// Arrays [], 0 index based
+
+const someData = ["john", "smith", "tom", "jessica", "jennifer", 34, "denzel"];
+
+console.log(someData);
+
+console.log(someData[2]);
+
+let bestFriend = someData[6];
+console.log(bestFriend);
+
+console.log(someData[8]);
+
+someData[5] = "daniel";
+console.log(someData);
+console.log(someData[5]);
