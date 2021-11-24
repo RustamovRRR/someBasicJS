16246da04cf202d5c20dd8e5fe7d1016d71aed67
RustamovRRR <rustamrrustamov@gmail.com>
2021-11-25 00:06:49 +0400

    more basic array methods and properties

--- arrayMethodAndProperties/app.js
+++ arrayMethodAndProperties/app.js
@@ -23,3 +23,18 @@ console.log(names);
 const newArray = ["Ben", "Tywin", "John", "Eddard"];
 const concatArrays = names.concat(newArray);
 console.log(concatArrays);
+
+//splice method - using splice to remove element. It modifies the array
+names.splice(1, 1);
+console.log(names);
+// using splice to remove and add elements
+names.splice(1, 2, "barbara", "giovanni", "carla");
+console.log(names);
+
+//slice method - slice() method creates a new array. It does not remove any elements from the source array.
+const sliceArray = names.slice(1, 3);
+console.log(sliceArray);
+
+//using one argument in slice
+const oneArgSlice = names.slice(2);
+console.log(oneArgSlice);
