90fd7573e5c47e331e8f2b21555f516a910bf029
RustamovRRR <rustamrrustamov@gmail.com>
2021-11-24 23:27:12 +0400

    using template literals

---
+++ templateLiterals/app.js
@@ -0,0 +1,10 @@
+// Template Literals ES6+
+// Backtick characters ``
+// Interpolation ${} - insert expression (value)
+
+const personName = "Anna";
+const age = 26;
+
+const result = `This is ${personName}. She is ${age} years old. 
+She loves music and reading books. Simple math ${22 + 12}`;
+console.log(result);
