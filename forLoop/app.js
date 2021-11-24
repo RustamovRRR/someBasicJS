fdd906e523ebb3c0e3bf4323d30383c1592b986f
RustamovRRR <rustamrrustamov@gmail.com>
2021-11-24 19:09:16 +0400

    working with for loop

---
+++ forLoop/app.js
@@ -0,0 +1,17 @@
+// using for loop
+
+for (let i = 0; i < 10; i++) {
+  console.log(`number is ${i}`);
+}
+
+console.log("------------- decrease -------------");
+
+for (let i = 10; i > 0; i--) {
+  console.log(`number is ${i}`);
+}
+
+console.log("----------- adding 2 ----------------");
+
+for (let i = 0; i < 10; i += 2) {
+  console.log(`number is ${i}`);
+}
