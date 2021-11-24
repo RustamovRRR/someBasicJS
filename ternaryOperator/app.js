5b163a5a4173e31e000afbc63b5dcc716a327539
RustamovRRR <rustamrrustamov@gmail.com>
2021-11-25 02:39:51 +0400

    simple ternary operator example

---
+++ ternaryOperator/app.js
@@ -0,0 +1,8 @@
+// ternary operator
+// condition ? (runs if true) : (run if false)
+
+const value = 10;
+
+value > 4
+  ? console.log("value is bigger than four")
+  : console.log("value is not bigger");
